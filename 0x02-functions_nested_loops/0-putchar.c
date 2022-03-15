06ca99887eee9131c9570ccefe600df00050f909
teddy004 <tewodrosatirsaw4@gmail.com>
2022-03-15 05:37:12 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * main - Print "Holberton" followed by a new line.
+ * main - Print "_putchar" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
 int main(void)
 {
-	char holberton[] = "Holberton";
+	char holberton[] = "_putchar";
 	int i = 0;
 
 	while (holberton[i] != '\0')
