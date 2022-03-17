7f3d4562e822bebf966088d9a4963b5dac4471d1
teddy004 <tewodrosatirsaw4@gmail.com>
2022-03-17 06:12:19 -0700

    error fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
         int i;
 
         printf("Infinite loop incoming :(\n");
-
-        i = 0;
 	/*
+        i = 0;
+
         while (i < 10)
         {
                 putchar(i);
