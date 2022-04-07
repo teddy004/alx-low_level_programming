86e0cd5a2f419fbf0a948128a283523f4888452f
amlaku <amlakuyalew@gmail.com>
2022-04-07 11:22:52 -0300

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -79,7 +79,7 @@ void *_calloc(unsigned int number, unsigned int size)
  */
 int main(int argc, char **argv)
 {
-	char *n1, *n2, *multi_res;
+	char *num1, *num2, *mul;
 	unsigned int l = 0, l1 = 0, l2 = 0, a, b, t = 0, c = 0, ten = 0;
 
 	if (argc < 3)
@@ -87,18 +87,18 @@ int main(int argc, char **argv)
 		print_string("Error");
 		exit(98);
 	}
-	n1 = argv[1];
-	n2 = argv[2];
-	if (!(check_num(n1) && check_num(n2)))
+	num1 = argv[1];
+	num2 = argv[2];
+	if (!(check_num(num1) && check_num(num2)))
 	{
 		print_string("Error");
 		exit(98);
 	}
-	l1 = string_length(n1);
-	l2 = string_length(n2);
+	l1 = string_length(num1);
+	l2 = string_length(num2);
 	l = l1 + l2;
-	multi_res = _calloc(l + 1, sizeof(char *));
-	if (multi_res == 0)
+	mmul= _calloc(l + 1, sizeof(char *));
+	if (mul == 0)
 	{
 		print_string("Error");
 		exit(98);
@@ -107,16 +107,16 @@ int main(int argc, char **argv)
 	{
 		for (c = 0, b = 0; b < l2; b++)
 		{
-			t = (n1[l1 - a - 1] - '0') * (n2[l2 - b - 1] - '0') + c;
+			t = (num1[l1 - a - 1] - '0') * (num2[l2 - b - 1] - '0') + c;
 			printf("%u\n", t);
-			if (multi_res[l - b - ten - 1] > 0)
-				t = t + multi_res[l - b - ten - 1] - '0';
-			multi_res[l - b - ten - 1] = t % 10 + '0';
+			if (mul[l - b - ten - 1] > 0)
+				t = t + mul[l - b - ten - 1] - '0';
+			mul[l - b - ten - 1] = t % 10 + '0';
 			c = t / 10;
 		}
-		multi_res[l - b - ten - 1] += c + '0';
+		mul[l - b - ten - 1] += c + '0';
 	}
-	print_string(multi_res);
-	free(multi_res);
+	print_string(mul);
+	free(mul);
 	return (0);
 }
