e9cda2cf4735b0c88fc1743477a935e57d96a007
teddy004 <tewodrosatirsaw4@gmail.com>
2022-04-13 05:56:23 -0700

    adding a _putchar prototype function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <stdlib.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
