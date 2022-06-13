5cc3b5dc54a877f29813d63c4b2d8370d673c6b9
teddy004 <tewodrosatirsaw4@gmail.com>
2022-06-13 12:18:45 +0000

    updating make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/sentayehut/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libinjection.so
+wget -P .. https://github.com/teddy004/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD="$PWD/../libinjection.so"
