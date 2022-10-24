f719476c3311958c72fe9436d5c30b549dd4d908
teddy004 <tewodrosatirsaw4@gmail.com>
2022-10-24 08:37:07 +0000

    updated print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,38 +1,36 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-* print_buffer - prints a buffer.
-* @b: pointer to string
-* @size: total number of bytes
-* Return: nothing
-**/
-
+ * print_buffer - Prints a buffer
+ * @b: The character buffer
+ * @size: The number of bytes to print from the character buffer
+ */
 void print_buffer(char *b, int size)
 {
-	int c, d;
+	int idx, col;
 
-	c = 0;
-	if (size <= 0)
+	for (idx = 0; idx < size; idx += 10)
 	{
-		printf("\n");
-	}
-
-	while (c < size)
-	{
-		printf("%8.8x:", c);
-		d = 0;
-		while (d < 10 )
+		printf("%08x: ", idx);
+		for (col = idx; col < idx + 10; col += 2)
 		{
-			printf("%02x", b[c + d]);
-			if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
-			{
-				printf(" ");
-			}
-			d++;
+			if (col < size)
+				printf("%02x", *(b + col));
+			else
+				printf("%2c", ' ');
+			if (col + 1 < size)
+				printf("%02x", *(b + col + 1));
+			else
+				printf("%2c", ' ');
+			putchar(' ');
 		}
-		c += 10;
-		printf("\n");
+		for (col = idx; col < idx + 10; col++)
+		{
+			if (col < size)
+				putchar(*(b + col) < ' ' ? '.' : *(b + col));
+		}
+		putchar('\n');
 	}
-
+	if (size <= 0)
+		putchar('\n');
 }
