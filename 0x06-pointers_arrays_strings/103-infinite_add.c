33517d91ec0704df11e607609ce2f376af282362
teddy004 <tewodrosatirsaw4@gmail.com>
2022-10-24 08:34:01 +0000

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,75 +1,51 @@
 #include "main.h"
 
 /**
-* _strlen - calculate string length
-* @a: string
-<<<<<<< HEAD
-* Return: lenght of string
-=======
-* Return: lengh
->>>>>>> 66a7e64280d884ae783b6ff5258e667ccd7a2047
-**/
-int _strlen(char *a)
-{
-	int i = 0;
-
-	while (*a != '\0')
-		i++, a++;
-	return (i);
-}
-
-/**
-* *infinite_add - adds two numbers
-<<<<<<< HEAD
-* @n1: string
-* @n2: string
-* @r: string
-* @size_r: int
-* Return: 0
-=======
-* @n1: number1
-* @n2: number2
-* @r: buffer to store result
-* @size_r: buffer size
-* Return: pointer to the result
->>>>>>> 66a7e64280d884ae783b6ff5258e667ccd7a2047
-**/
+ * infinite_add - Adds two numbers
+ * @n1: The first number
+ * @n2: The second number
+ * @r: The buffer for storing the result
+ * @size_r: The size of the buffer
+ *
+ * Return: If result can be stored in r, then r, otherwise 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int k, n2_s, n1_s, m;
+	int n1_len, n2_len, max_len, i;
+	char n1_dig, n2_dig, carry, rem, tmp;
 
-	r[size_r] = '\0';
-	k = 0;
-	m = 0;
-	n1_s = _strlen(n1);
-	n2_s = _strlen(n2);
-	size_r--;
-	if (n1_s > size_r || n2_s > size_r || size_r == 0 ||
-	    (n1_s == size_r && n2_s == size_r &&
-	     (n1[0] - '0') + (n2[0] - '0') > 9))
-	{
+	for (n1_len = 0; *(n1 + n1_len) != '\0'; n1_len++)
+		carry = 0;
+	for (n2_len = 0; *(n2 + n2_len) != '\0'; n2_len++)
+		rem = 0;
+	max_len = n1_len > n2_len ? n1_len : n2_len;
+	i = max_len;
+	if (size_r < max_len + 1)
 		return (0);
-	}
-	n1_s--;
-	n2_s--;
-	while (n1_s >= 0 || n2_s >= 0)
+	*(r + max_len) = '\0';
+	i--;
+	n1_len--;
+	n2_len--;
+	for (; i >= 0; i--)
 	{
-		if (n1_s < 0)
-			m = n2[n2_s] - '0' + k;
-		else if (n2_s < 0)
-			m = n1[n1_s] - '0' + k;
-		else
-			m = (n1[n1_s] - '0') + (n2[n2_s] - '0') + k;
-		r[size_r] = (m % 10) + '0';
-		k = m / 10;
-		n1_s--, n2_s--, size_r--;
+		n1_dig = n1_len >= 0 ? *(n1 + n1_len) - '0' : 0;
+		n2_dig = n2_len >= 0 ? *(n2 + n2_len) - '0' : 0;
+		rem = (n1_dig + n2_dig + carry) % 10;
+		carry = (n1_dig + n2_dig + carry) / 10;
+		*(r + i) = (rem + '0');
+		n1_len--;
+		n2_len--;
 	}
-	if (size_r > 0 && (k < 9 && k > 0))
+	if (carry > 0 && size_r >= max_len + 2)
 	{
-		r[size_r] = k + '0';
-		return (r + size_r);
+		for (i = max_len + 1; i > 0; i--)
+		{
+			tmp = *(r + i);
+			*(r + i) = *(r + i - 1);
+			*(r + i - 1) = tmp;
+		}
+		*(r + i) = carry + '0';
+		return (r);
 	}
-	else if (k == 0)
-		return (r + size_r);
-	return (0);
+	return ((carry > 0 && size_r < max_len + 2) ? 0 : r);
 }
