058f4fa8fd0e62f66434de652a488d3de667766a
teddy004 <tewodrosatirsaw4@gmail.com>
2022-10-24 07:49:07 +0000

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,52 +1,53 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number - prints an integer
+ * @n: the integer to print
  */
 void print_number(int n)
 {
-<<<<<<< HEAD
-	int i, d, p, f;
-	int j;
-	char num[1000];
-=======
-	unsigned int z;
-	int m, b;
->>>>>>> 02f4879befaee9c324521caa68f89e7cfec064bb
+	int i;
+	int max;
+	int minVal = n + 1 == -2147483640 - 7 ? -1 : 0;
+	int rem = n < 0 ? 0 - n + minVal : n;
 
-	b = 10;
+	max = 1;
+	max *= 100000;
+	max *= 100000;
 
-	if (n < 10 && n >= 0)
-	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
+	if (n < 0)
 		_putchar('-');
-		_putchar (n + '0');
-	}
 
-	else
+	for (i = 9; i >= 0; i--)
 	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
+		int power = b10_pow(i);
+
+		if (rem > power - 1 && !(i == 0 && minVal < 0))
+			_putchar((int)((rem / power) % 10) + '0');
+		else if (i == 0 && minVal >= 0)
+			_putchar((int)((rem / power) % 10) + '0');
+		if (i == 0 && minVal < 0)
+			_putchar('8');
+	}
+}
+
+/**
+ * b10_pow - Computes the power of 10 to the given index
+ * @idx: The index to which 10 is raised to.
+ *
+ * Return: A power of 10
+ */
+int b10_pow(int idx)
+{
+	int i;
+	int result = 1;
+
+	if (idx < 0)
+		return (0);
+
+	for (i = 0; i < idx; i++)
 	{
-		b = b * 10;
+		result *= 10;
 	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	return (result);
 }
